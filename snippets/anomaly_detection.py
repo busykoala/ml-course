0e27ebc8326293427727b1e32ed111cc6cdcac65
Matthias Osswald <matthias@busykoala.io>
2022-12-27 15:29:04 +0100

    Add density plot of data.

--- snippets/anomaly_detection.py
+++ snippets/anomaly_detection.py
@@ -1,14 +1,27 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+from matplotlib import pyplot as plt
+from mayavi import mlab
+from scipy import stats
 from scipy.stats import multivariate_normal
 from scipy.stats import norm
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import OneClassSVM
 import numpy as np
 import numpy as np
 
 
+def normalize(X):
+    scaler = StandardScaler()
+    scaler.fit(X)
+    return scaler.transform(X)
+
+
 def gaussian(X, y, threshold):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     mu = np.mean(X_train, axis=0)
     sigma = np.std(X_train, axis=0)
     probabilities = norm.pdf(X_test, mu, sigma)
@@ -21,7 +34,7 @@ def gaussian(X, y, threshold):
 
 
 def multivariate_gaussian(X, y, threshold):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     mu = np.mean(X_train, axis=0)
     sigma = np.cov(X_train.T)
     probabilities = multivariate_normal.pdf(X_test, mean=mu, cov=sigma)
@@ -34,7 +47,7 @@ def multivariate_gaussian(X, y, threshold):
 
 
 def one_class_svm_example(X, y, threshold):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     model = OneClassSVM(kernel='rbf', nu=threshold)
     model.fit(X_train)
     y_pred = model.predict(X_test)
@@ -43,11 +56,30 @@ def one_class_svm_example(X, y, threshold):
     print("-" * 50)
 
 
+def plot_density_map(X_clean, X_dirty):
+    xyz = np.vstack(X_clean.T)
+    kde = stats.gaussian_kde(xyz)
+    density_one = kde(xyz)
+    xyz = np.vstack(X_dirty.T)
+    kde = stats.gaussian_kde(xyz)
+    density_two = kde(xyz)
+    mlab.figure('DensityPlot without anomalies')
+    mlab.points3d(*X_clean.T, density_one, scale_mode='none', scale_factor=0.07)
+    mlab.axes()
+    mlab.figure('DensityPlot with anomalies')
+    mlab.points3d(*X_dirty.T, density_two, scale_mode='none', scale_factor=0.07)
+    mlab.axes()
+    mlab.show()
+
+
 if __name__ == '__main__':
     anomaly_size = 100
     X_size = (10000, 3)
     X = np.random.normal(size=X_size)
+    X = normalize(X)
+    X_clean = X.copy()
     X[:anomaly_size] = np.random.uniform(low=-5, high=5, size=(anomaly_size, X_size[1]))
+    plot_density_map(X_clean, X)
     y = np.ones(X.shape[0])
     y[:anomaly_size] = -1
     threshold = 0.01
